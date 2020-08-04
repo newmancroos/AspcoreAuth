d1196942f257f280c650870c1544b49be70c7ea9
newmancroos <newmancroos@gmail.com>
2020-08-04 15:55:42 -0400

    Reister and email verification

---
+++ 2.IdentitySample/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace _2.IdentitySample.Data
+{
+    public class AppDbContext : IdentityDbContext 
+        // Since we use Identity we need to use IdentityDbContext instead of  DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
