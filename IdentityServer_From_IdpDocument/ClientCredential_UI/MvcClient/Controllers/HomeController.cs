93d77791f76c3c0cb380d18eabfb4ddf074275f5
newmancroos <newmancroos@gmail.com>
2021-02-08 20:09:16 -0500

    MVC client with UI authentication

---
+++ IdentityServer_From_IdpDocument/ClientCredential_UI/MvcClient/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MvcClient.Models;
+using System.Diagnostics;
+
+namespace MvcClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [Authorize]  // or we can enable start up .RequireAuthorization() for global authorization
+        public IActionResult SecretMethod()
+        {
+            return View();
+        }
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public IActionResult Logout()
+        {
+            return SignOut("Cookie", "oidc");
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ IdentityServer_From_IdpDocument/ClientCredential_UI/src/IdentityServer/Config.cs
@@ -0,0 +1,58 @@
+﻿
+using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            { 
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email(),
+                new IdentityResource("customprofile","Custom Profile", new List<string>{"userstatus","email"})
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
+            { new ApiScope("api1", "My API")
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+
+                    //No interact users so use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = {"api1" }
+                },
+                new Client
+                {
+                    ClientId="mvc",
+                    ClientSecrets = {new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.Code,
+                    //where to redirect after login
+                    RedirectUris = {"https://localhost:44389/signin-oidc" },
+                    //where to redirect after logout
+                    PostLogoutRedirectUris = { "https://localhost:44389/signout-callback-oidc" },
+                    AllowedScopes = new List<string>
+                    { 
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        "customprofile"
+                    }
+                }
+            };
+    }
+}
